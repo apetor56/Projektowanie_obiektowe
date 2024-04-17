59f64035e8d5ce742834da525cc693660e92bdff
apetor56 <lukten.github@gmail.com>
2024-04-17 15:52:29 +0200

    part 4 - autowired singleton instance

--- 03_wzorce_kreacyjne/src/main/kotlin/com/example/_03_wzorce_kreacyjne/controller/Controller.kt
+++ 03_wzorce_kreacyjne/src/main/kotlin/com/example/_03_wzorce_kreacyjne/controller/Controller.kt
@@ -9,9 +9,13 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.beans.factory.annotation.Autowired
 
 @RestController
-class Controller(private val authenticationManager: AuthenticationManager) {
+class Controller() {
+    @Autowired
+    lateinit var authenticationManager: AuthenticationManager;
+
     @GetMapping("/")
     fun getAllProducts(@RequestBody loginRequest: LoginRequest): ResponseEntity<Any> {
         if(authenticationManager.authenticate(loginRequest.login, loginRequest.password))
